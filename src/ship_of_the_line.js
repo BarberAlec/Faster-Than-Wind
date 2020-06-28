b0fabb3c4bef383d79bf0fdb8b487ad2509c4151
Alec Barber <barberal@tcd.ie>
2020-06-28 13:34:32 +0100

    cool cannon ball anim

--- src/ship_of_the_line.js
+++ src/ship_of_the_line.js
@@ -5,6 +5,8 @@ make_enemy_ship();
 make_cannon('19%');
 make_cannon('26%');
 
+fire_cannon()
+
 // can1 = new my_cannon('18%');
 // can2 = new my_cannon('25%');
 // can1.draw();
@@ -39,13 +41,53 @@ function make_cannon(topMargin) {
     cannon_button.style.position = "absolute";
     cannon_button.style.marginLeft = '38%';
     cannon_button.style.marginTop = topMargin;
-    
-
-    //cannon_button.style.lineHeight = "60px";
-    //cannon_button.style.fontWeight = "bold";
-    //cannon_button.style.background = "salmon";
-    
 }   
+
+function makecannon_ball(topMargin,leftMargin){
+    var ball = document.createElement("cannon_ball");
+    ball.innerHTML = '<img src="../assets/img/cannon_ball.png" width="25"/>';
+
+    var body = document.getElementsByTagName("body")[0];
+
+    body.appendChild(ball);
+    ball.style.border = "none";
+    ball.style.position = "absolute";
+    ball.style.marginLeft = leftMargin;
+    ball.style.marginTop = topMargin;
+    return ball
+}
+
+function fire_cannon(){
+    ball = makecannon_ball('20%','48%')
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos > 370) {
+            clearInterval(id);
+            ball.parentNode.removeChild(ball)
+            ball_attack = makecannon_ball('3%','70%')
+            var pos_att = 0;
+            var id_att = setInterval(frame_attack, 5);
+            function frame_attack(){
+                if (pos_att == 195) {
+                    clearInterval(id_att);
+                    ball_attack.parentNode.removeChild(ball_attack)
+                }
+                else{
+                    pos_att++;
+                    ball_attack.style.left = pos_att + "px";
+                    ball_attack.style.top = pos_att + "px"; 
+                }
+            }
+
+        }
+        else{
+            pos = pos + 1.41; 
+            //ball.style.top = pos + "px"; 
+            ball.style.left = pos + "px"; 
+        }
+    }
+}
 
 // class my_cannon{
 //     constructor(v){
