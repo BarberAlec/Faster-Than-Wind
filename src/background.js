37d7ed945ffb5cd078dce73740e215b88031fc62
Harry Collins <hcollins345@gmail.com>
2020-06-28 02:05:23 +0100

    fix previous commit

--- src/background.js
+++ src/background.js
@@ -17,15 +17,15 @@ ctx.fillRect(50, 50, 100, 150);
 ctx.fillStyle = 'rgba(0, 255, 0, .5)';
 ctx.fillRect(75, 75, 100, 100);
 
-let left_top_corner_x = canvas.width * 0.02;
-let right_top_corner_x = canvas.width * 0.96;
-let left_top_corner_y = canvas.height * 0.02;
-let right_top_corner_y = canvas.height * 0.96;
+let left_top_corner_x = width * 0.02;
+let right_top_corner_x = width * 0.96;
+let left_top_corner_y = height * 0.02;
+let right_top_corner_y = height * 0.96;
 
 ctx.lineWidth = 5;
 ctx.strokeStyle = 'rgb(255, 255, 255)';
 ctx.strokeRect(
-    0.02*canvas.width,
-    0.02*canvas.height,
-    canvas.width - 0.08*canvas.width, 
-    canvas.height - 0.08*canvas.height);
+    0.02*width,
+    0.02*height,
+    width - 0.08*width, 
+    height - 0.08*height);
