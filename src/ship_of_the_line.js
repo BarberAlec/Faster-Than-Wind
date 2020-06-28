54a84194473b0302643372be7e58424a79c6d191
Alec Barber <barberal@tcd.ie>
2020-06-28 13:01:20 +0100

    new connons on friend ship

--- src/ship_of_the_line.js
+++ src/ship_of_the_line.js
@@ -2,8 +2,13 @@
 
 make_our_ship();
 make_enemy_ship();
-make_cannon('18%');
-make_cannon('25%');
+make_cannon('19%');
+make_cannon('26%');
+
+// can1 = new my_cannon('18%');
+// can2 = new my_cannon('25%');
+// can1.draw();
+// can2.draw();
 
 function make_our_ship() {
     base_image_rot = new Image();
@@ -24,7 +29,7 @@ function make_enemy_ship() {
 
 function make_cannon(topMargin) {
     var cannon_button = document.createElement("cannon");
-    cannon_button.innerHTML = '<img src="../assets/img/small_cannon.png" width="130"/>';
+    cannon_button.innerHTML = '<img src="../assets/img/cannon.png" width="180"/>';
 
     //cannon_button = '../assets/img/small_cannon.png';
     var body = document.getElementsByTagName("body")[0];
@@ -32,7 +37,7 @@ function make_cannon(topMargin) {
     body.appendChild(cannon_button);
     cannon_button.style.border = "none";
     cannon_button.style.position = "absolute";
-    cannon_button.style.marginLeft = '40%';
+    cannon_button.style.marginLeft = '38%';
     cannon_button.style.marginTop = topMargin;
     
 
@@ -41,3 +46,22 @@ function make_cannon(topMargin) {
     //cannon_button.style.background = "salmon";
     
 }   
+
+// class my_cannon{
+//     constructor(v){
+//         this.margin = v;
+//     }
+//     draw(){
+//         var cannon_button = document.createElement("cannon");
+//         cannon_button.innerHTML = '<img src="../assets/img/small_cannon.png" width="130"/>';
+//         var body = document.getElementsByTagName("body")[0];
+
+//         body.appendChild(cannon_button);
+//         cannon_button.style.border = "none";
+//         cannon_button.style.position = "absolute";
+//         cannon_button.style.marginLeft = '40%';
+//         cannon_button.style.marginTop = this.margin;
+//     }
+    
+
+// }
