012de8bf30d65b63d194da0650f67e0b12cfeccc
Alec Barber <barberal@tcd.ie>
2020-06-30 23:21:05 +0100

    make all cannons and ships into classes

--- src/preamble.js
+++ src/preamble.js
@@ -1,8 +1,9 @@
-// This file draws some ships
-make_our_ship();
-make_enemy_ship();
+
+
+ship_enemy = new ship_enemy();
+ship_friend = new ship_friend();
 
 
 // Make our cannons
-can1 = new my_cannon('18%');
-can2 = new my_cannon('25%');
+can1 = new cannon_friend('18%');
+can2 = new cannon_friend('25%');
