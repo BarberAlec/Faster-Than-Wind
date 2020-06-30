3f4aca19b6333a76e4e272647e9c8047de81fc7b
Alec Barber <barberal@tcd.ie>
2020-06-30 21:51:32 +0100

    this

---
+++ src/preamble.js
@@ -0,0 +1,8 @@
+// This file draws some ships
+make_our_ship();
+make_enemy_ship();
+
+
+// Make our cannons
+can1 = new my_cannon('18%');
+can2 = new my_cannon('25%');
--- src/ship_of_the_line.js
+++ src/ship_of_the_line.js
@@ -1,16 +1,66 @@
-// This file draws some ships
 
-make_our_ship();
-make_enemy_ship();
-make_cannon('19%');
-make_cannon('26%');
 
-// fire_cannon()
+class my_cannon{
+    constructor(v){
+        this.margin = v;
+        this.cannon_button = document.createElement("cannon");
+        this.cannon_button.innerHTML = '<img src="../assets/img/cannon.png" width="180"/>';
+        this.body = document.getElementsByTagName("body")[0];
 
-// can1 = new my_cannon('18%');
-// can2 = new my_cannon('25%');
-// can1.draw();
-// can2.draw();
+        this.body.appendChild(this.cannon_button);
+        this.cannon_button.style.border = "none";
+        this.cannon_button.style.position = "absolute";
+        this.cannon_button.style.marginLeft = '38%';
+        this.cannon_button.style.marginTop = this.margin;
+    }
+    _makecannon_ball(topMargin,leftMargin){
+        var ball = document.createElement("cannon_ball");
+        ball.innerHTML = '<img src="../assets/img/cannon_ball.png" width="25"/>';
+    
+        this.body.appendChild(ball);
+        ball.style.border = "none";
+        ball.style.position = "absolute";
+        ball.style.marginLeft = leftMargin;
+        ball.style.marginTop = topMargin;
+        return ball
+    }
+
+    fire_cannon(){
+        this.ball = this._makecannon_ball('20%','48%')
+        this.pos = 0;
+        var t = this;
+        this.id = setInterval(function(){t._frame();}, 5);
+    }
+
+    _frame_attack(){
+        if (this.pos_att == 195) {
+            clearInterval(this.id_att);
+            this.ball_attack.parentNode.removeChild(this.ball_attack)
+        }
+        else{
+            this.pos_att++;
+            this.ball_attack.style.left = this.pos_att + "px";
+            this.ball_attack.style.top = this.pos_att + "px"; 
+        }
+    }
+
+    _frame() {
+        if (this.pos > 370) {
+            clearInterval(this.id);
+            this.ball.parentNode.removeChild(this.ball)
+            this.ball_attack = this._makecannon_ball('3%','70%')
+            this.pos_att = 0;
+            var t = this;
+            this.id_att = setInterval(function(){t._frame_attack()}, 5);
+        }
+        else{
+            this.pos = this.pos + 1.41;
+            this.ball.style.left = this.pos + "px";
+        }
+    }
+}
+
+
 
 function make_our_ship() {
     base_image_rot = new Image();
@@ -89,21 +139,3 @@ function fire_cannon(){
     }
 }
 
-// class my_cannon{
-//     constructor(v){
-//         this.margin = v;
-//     }
-//     draw(){
-//         var cannon_button = document.createElement("cannon");
-//         cannon_button.innerHTML = '<img src="../assets/img/small_cannon.png" width="130"/>';
-//         var body = document.getElementsByTagName("body")[0];
-
-//         body.appendChild(cannon_button);
-//         cannon_button.style.border = "none";
-//         cannon_button.style.position = "absolute";
-//         cannon_button.style.marginLeft = '40%';
-//         cannon_button.style.marginTop = this.margin;
-//     }
-    
-
-// }
