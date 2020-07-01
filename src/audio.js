3e29c39ee78deb166358762e9eca54cd6ba2e956
fergus <59059263+C-Fergus@users.noreply.github.com>
2020-07-01 22:16:21 +0100

    create audio.js

---
+++ src/audio.js
@@ -0,0 +1,20 @@
+var Music = document.getElementById("Music");
+
+var Music_Toggle = document.createElement("BUTTON");
+
+Music_Toggle.onclick = Music.play();
+
+// var music = new Audio(),
+// 	i = 0;
+// var playlist = new Array('assets/music/barbara_allen.wav');
+//
+// audio.addEventListener('ended', function () {
+//     i = ++i < playlist.length ? i : 0;
+//     console.log(i);
+//     audio.src = playlist[i];
+//     audio.play();
+// }, true);
+// audio.volume = 1;
+// audio.loop = false;
+// audio.src = playlist[0];
+// audio.play();
