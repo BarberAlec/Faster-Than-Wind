05f7ed0d0c8866224a29a57cb739f1a7f895feeb
fergus <59059263+C-Fergus@users.noreply.github.com>
2020-07-02 22:09:34 +0100

    create splash screen

--- src/splash.js
+++ src/splash.js
@@ -1,9 +1,18 @@
-var canvas = document.querySelector('.myCanvas');
+var canvas = document.getElementById("screen");
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = (9/16)*width;
+const game_width = canvas.width = window.innerWidth;
+const game_height = canvas.height = (9/16)*game_width;
 
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, width, height);
+ctx.fillRect(0, 0, game_width, game_height);
+
+function draw_logo() {
+    logo = new Image();
+    logo.src = '../assets/img/logo.png';
+    logo.onload = function () {
+        ctx.drawImage(logo, (canvas.width - logo.width)/2, (game_height - logo.height)/4);
+    };
+}
+draw_logo();
